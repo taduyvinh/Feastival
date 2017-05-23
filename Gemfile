4351d99a16171298a958b6eaf3b7affde769cd13
taduyvinh <taduyvinh@gmail.com>
2017-05-23 11:56:02 +0700

    Add gem config

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "i18n-js"
 gem "kaminari"
 gem "carrierwave"
 gem "devise"
+gem "config"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_messages
+    safe_join(flash.map do |type, message|
+      content_tag :div, message, class: flash_type(type).to_s
+    end)
+  end
+
+  def flash_type type
+    Settings.flash_type[type.to_sym]
+  end
 end
