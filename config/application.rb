e7cc8715e939b45d1c1e1775ff84fbfbc40bc2ce
taduyvinh <taduyvinh@gmail.com>
2017-07-13 09:28:59 +0700

    init api

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,14 @@ Bundler.require(*Rails.groups)
 module Feastival
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**",
+    "*.{rb,yml}")]
+    config.active_record.time_zone_aware_types = [:datetime, :time]
+
     config.generators do |generator|
       generator.test_framework :rspec
     end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
